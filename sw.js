9a7a7dd0e4e060e2d83e40c5122ed40a8fe8d675
mkb-kasugai <blackcat2002k@gmail.com>
2026-01-05 21:59:30 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,10 @@
-const CACHE_NAME = "mkb-bus-app-v4";
+const CACHE_NAME = "mkb-bus-app-v5";
 const ASSETS = [
   "./",
   "./index.html",
   "./style.css",
   "./manifest.json",
-  "./sw.js",
-  "./icon-192.png",
-  "./icon-512.png"
+  "./sw.js"
 ];
 
 self.addEventListener("install", (event) => {
@@ -19,9 +17,9 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => (k === CACHE_NAME ? null : caches.delete(k)))))
-      .then(() => self.clients.claim())
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k === CACHE_NAME ? null : caches.delete(k))))
+    ).then(() => self.clients.claim())
   );
 });
 
